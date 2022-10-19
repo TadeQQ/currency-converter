8df5fbe23c67ebc8348fac5b913d546201da3155
TadeQ <tadeuszkiszka1@gmail.com>
2022-10-19 17:12:55 +0200

    style App.tsx

---
+++ src/App.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: gray;
+  border: none;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  align-items: center;
+`;
