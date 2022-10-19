0b3e29a55e3d589448f3e67d388428ae0cca4824
TadeQ <tadeuszkiszka1@gmail.com>
2022-10-20 00:29:50 +0200

    implement ApiClient on CurrenciesSerivces.ts

---
+++ src/services/CurrenciesServices.ts
@@ -0,0 +1,54 @@
+import { apiClient } from "../libs/axios";
+
+type Imiona = "Marek" | "Maciek";
+
+type Obj = Record<Imiona, boolean | number>;
+
+interface FetchAllCurrenciesResponse {
+  success: boolean;
+  symbols: Record<string, string>;
+}
+
+const fetchAllCurrencies = async () => {
+  const res = await apiClient.get<FetchAllCurrenciesResponse>("/symbols");
+  return res.data;
+};
+
+interface ConvertCurrenciesData {
+  from: string;
+  to: string;
+  amount: string;
+}
+export interface ConvertCurrenciesResponse {
+  date: string;
+  historical: string;
+  info: {
+    rate: number;
+    timestamp: number;
+  };
+  query: {
+    amount: number;
+    from: string;
+    to: string;
+  };
+  result: number;
+  success: boolean;
+}
+
+const convertCurrencies = async ({
+  from,
+  to,
+  amount,
+}: ConvertCurrenciesData) => {
+  const res = await apiClient.get<ConvertCurrenciesResponse>("/convert", {
+    params: { from, to, amount },
+  });
+  return res.data;
+};
+
+export const CurrenciesServices = {
+  convertCurrencies,
+  fetchAllCurrencies,
+};
+
+useQuery(["currencies"], CurrenciesServices.fetchAllCurrencies);
