6f332857970274460635668f693c7d60b6b859a9
TadeQ <tadeuszkiszka1@gmail.com>
2022-10-23 17:58:03 +0200

    Update axios.ts

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const apiClient = axios.create({
   baseURL: "https://api.apilayer.com/exchangerates_data",
   headers: {
-    apiKey: "maGGoEXvLaU6eLv0YNHJBRmXZweG0Rhe", // process.env.REACT_APP_API_KEY as string
+    apiKey: process.env.REACT_APP_API_KEY as string,
   },
 });
