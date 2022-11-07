0f62a0a1330aba2b694da7bc2e4485ec294bb87f
TadeQ <tadeuszkiszka1@gmail.com>
2022-11-07 18:22:15 +0100

    Update App.tsx

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header } from "../components/Header/Header";
-import { Container } from "../App.styled";
+import { Container } from "../components/styles/App.styled";
 import { Converter } from "../components/Converter/Converter";
 import { Outlet, Link } from "react-router-dom";
 import { Routes } from "./Routes";
