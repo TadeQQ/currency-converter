f453a32e402e5779bb293e677890a58688cd8c42
TadeQ <tadeuszkiszka1@gmail.com>
2022-11-09 22:57:59 +0100

    Update Historypage with working hook

--- src/routes/HistoryPage.tsx
+++ src/routes/HistoryPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLoaderData, LoaderFunctionArgs } from "react-router-dom";
+import { useHistoryConvert } from "../Hooks/useHistoryConvert";
 
 export const historyPageLoader = (loaderArgs: LoaderFunctionArgs) => {
   console.log(loaderArgs.params);
@@ -8,7 +9,15 @@ export const historyPageLoader = (loaderArgs: LoaderFunctionArgs) => {
 
 export const HistoryPage = () => {
   const data = useLoaderData();
+  const { history } = useHistoryConvert();
 
   console.log(data);
-  return <div>HistoryPage</div>;
+  return (
+    <div>
+      HistoryPage
+      {history.map((el) => (
+        <li key={el.info.timestamp}>{JSON.stringify(el.query)}</li>
+      ))}
+    </div>
+  );
 };
