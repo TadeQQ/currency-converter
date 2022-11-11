5e470f485768a1d0a2f4b1958988f33588f5d8aa
TadeQ <tadeuszkiszka1@gmail.com>
2022-11-11 23:56:16 +0100

    Update App.tsx

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Header } from "../components/Header/Header";
 import { Container } from "../components/styles/App.styled";
-import { Converter } from "../components/Converter/Converter";
 import { Outlet, Link } from "react-router-dom";
 import { Routes } from "./Routes";
 export function App() {
@@ -10,7 +9,6 @@ export function App() {
       <Header />
       <Link to={Routes.HISTORY}>Historical page</Link>
       <Outlet />
-      {/* <Converter /> */}
     </Container>
   );
 }
