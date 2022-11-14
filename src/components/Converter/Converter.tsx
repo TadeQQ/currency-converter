6a86e1ee34725c6cd54bbec1eb73c272db01b7f1
TadeQ <tadeuszkiszka1@gmail.com>
2022-11-14 14:20:34 +0100

    change styled tags names

--- src/components/Converter/Converter.tsx
+++ src/components/Converter/Converter.tsx
@@ -1,21 +1,14 @@
 import React from "react";
-import {
-  StyledConverter,
-  Form,
-  Div,
-  Input,
-  Convert,
-} from "../styles/Converter.styled";
+import { ConverterStyles } from "../styles/Converter.styled";
 import { SelectCurrency } from "../SelectCurrency/SelectCurrency";
 import { useFetchCurrencies } from "../../Hooks/useFetchCurrencies";
 import { useConvertCurrency } from "../../Hooks/convert/useConvertCurrency";
-import { useForm, Controller } from "react-hook-form";
-import { useNavigate, useLocation } from "react-router-dom";
+import { Controller } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { Routes } from "../../routes";
 export const Converter = () => {
-  const location = useLocation();
   const navigate = useNavigate();
-  const { currencies, isLoading } = useFetchCurrencies({});
+  const { currencies } = useFetchCurrencies({});
   const {
     form: {
       register,
@@ -23,28 +16,24 @@ export const Converter = () => {
       control,
       formState: { errors },
     },
-    query,
   } = useConvertCurrency({
     onError: () => console.log(errors, "błąd"),
     onSuccess: () => navigate(Routes.HISTORY),
-    //  (data) => {
-    //   console.log(data);
-    // },
   });
 
   return (
-    <StyledConverter>
-      <Form onSubmit={handleConvert}>
-        <Div>
-          <div>Amount</div>
-          <Input
+    <ConverterStyles.StyledConverter>
+      <ConverterStyles.Form onSubmit={handleConvert}>
+        <ConverterStyles.Div>
+          <label>Amount</label>
+          <ConverterStyles.Input
             type="number"
             placeholder="amount"
             {...register("amount", { valueAsNumber: true })}
           />
           {errors.amount?.message}
-        </Div>
-        <Div>
+        </ConverterStyles.Div>
+        <ConverterStyles.Div>
           <label htmlFor="from">From</label>
           <Controller
             control={control}
@@ -57,10 +46,9 @@ export const Converter = () => {
               />
             )}
           />
-
           {errors.from?.message}
-        </Div>
-        <Div>
+        </ConverterStyles.Div>
+        <ConverterStyles.Div>
           <label>To</label>
           <Controller
             control={control}
@@ -74,10 +62,9 @@ export const Converter = () => {
             )}
           />
           {errors.to?.message}
-        </Div>
-        <Convert type="submit">Convert</Convert>
-      </Form>
-      <div></div>
-    </StyledConverter>
+        </ConverterStyles.Div>
+        <ConverterStyles.Convert type="submit">Convert</ConverterStyles.Convert>
+      </ConverterStyles.Form>
+    </ConverterStyles.StyledConverter>
   );
 };
