7c4b1e19dfbce6d9a5a05156c8d83b1465479a64
TadeQ <tadeuszkiszka1@gmail.com>
2022-11-16 00:47:24 +0100

    add styling to Navbar

---
+++ src/components/Navbar/Navbar.styled.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-around;
+  background-color: #282c34;
+  color: white;
+  border-radius: 5px;
+  margin-top: 30px;
+  font-size: 20px;
+  font-family: sans-serif;
+  padding: 10px;
+  font-weight: bold;
+`;
+
+const Route = styled.a`
+  color: white;
+  margin: 5px;
+  transition: transform 0.2s;
+  :hover {
+    color: #61dafb;
+    transform: scale(1.5);
+  }
+`;
+
+export const StyledNav = {
+  Nav,
+  Route,
+};
