271e0fc21337a3fc0b2839f63ce175ca61c16d55
TadeQ <tadeuszkiszka1@gmail.com>
2022-11-16 00:47:15 +0100

    Create Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { StyledNav } from "./Navbar.styled";
+import { Routes } from "../../routes";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <StyledNav.Nav>
+      <Link to={Routes.CONVERTER} style={{ textDecoration: "none" }}>
+        <StyledNav.Route>Converter</StyledNav.Route>
+      </Link>
+      |
+      <Link to={Routes.HISTORY} style={{ textDecoration: "none" }}>
+        <StyledNav.Route>Converts history</StyledNav.Route>
+      </Link>
+    </StyledNav.Nav>
+  );
 };
