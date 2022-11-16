c586029fae66545a30411ee98f85a9860098fa8e
TadeQ <tadeuszkiszka1@gmail.com>
2022-11-17 00:04:04 +0100

    add predicate option to useQueriesData

--- src/routes/HistoryPage.tsx
+++ src/routes/HistoryPage.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useLoaderData, LoaderFunctionArgs } from "react-router-dom";
 import { Historical } from "../components/Historical/Historical";
-
+import { queryClient } from "../index";
+import { ConvertCurrenciesResponse } from "../services/CurrenciesServices";
 export const historyPageLoader = (loaderArgs: LoaderFunctionArgs) => {
   // console.log(loaderArgs.params);
-  return "historical";
+  const queries = queryClient.getQueriesData<ConvertCurrenciesResponse>({
+    queryKey: ["convert"],
+    predicate: (query) => Boolean(query.state.data),
+  });
+  const data = queries.map((query) => query[1]) as ConvertCurrenciesResponse[];
+  return data;
 };
 export const HistoryPage = () => {
   return (
