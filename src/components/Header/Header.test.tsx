057895547647aec8a5d4d6b9b0de5e9f03df43a5
TadeQ <tadeuszkiszka1@gmail.com>
2023-01-22 11:35:30 +0100

    test Header if exists any h2

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+
+import { Header } from './Header';
+
+describe('Header tests', () => {
+  beforeAll(() => {
+    console.log('test');
+  });
+
+  it('render component', () => {
+    render(<Header />);
+
+    expect(
+      screen.getByText('Welcome to your favorite currency converter')
+    ).toBeInTheDocument();
+  });
+
+  it('check if H2 exist', () => {
+    render(<Header />);
+
+    expect(
+      screen.getByText('Welcome to your favorite currency converter', {
+        selector: 'h2',
+      })
+    ).toBeInTheDocument();
+  });
+});
