f377452e5c37a3579d564e4df4dd0a78d3846cd6
TadeQ <tadeuszkiszka1@gmail.com>
2023-01-27 14:52:49 +0100

    feat: set config of Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: 'create-react-app',
+      bundler: 'webpack',
     },
   },
 });
