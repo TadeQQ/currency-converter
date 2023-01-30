031a00c4a6a6d76c3c4d712cc731a1f98a1b448e
TadeQ <tadeuszkiszka1@gmail.com>
2023-01-30 21:18:48 +0100

    refactor: delete unnecessary lines

--- src/Hooks/useHistoryConvert.ts
+++ src/Hooks/useHistoryConvert.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
+import { useState, useCallback } from 'react';
 import { useQueryClient, useIsRestoring } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { useLoaderData } from 'react-router-dom';
@@ -13,58 +13,10 @@ export const useHistoryConvert = () => {
 
   // const [history, setHistory] = useState<ConvertCurrenciesResponse[]>([]);
 
-  // const ref - useRef();
-
-  //   const test = {
-  //     wlasciowsc1: "tst",
-  //     wlasiciwcs2: "sdsd",
-  //   };
-
-  //   useEffect(() => {
-  //     console.log("c")
-  //   }); // co render
-
-  //   useEffect(() => {
-  //     console.log("c")
-  //   }, []); // po pierwszym renderze
-
-  //   useEffect(() => {
-  //     console.log("c")
-  //   }, [test.wlasciowsc1]);
-
-  //   useEffect(() => {
-  // (async () => {
-  //   fetch("/");
-  // })();
-
-  // const add = (x) => {
-  //   return (y) => {
-  //     return x+y;
-  //   }
-  // }
-
-  // add(1)(2);
-
-  // const addWith1 = add(1);
-
-  // const test = () => {
-  //   console.log("a");
-
-  //   setTimeout(() => {
-  //     console.log("b")
-  //   }, 0)
-
-  //   console.log("c")
-  // } // a, c, b
-
-  // addWith1(2)
-  //   }, [])
-
   const clearHistory = useCallback(() => {
     queryClient.removeQueries({ queryKey: ['convert'] });
     navigate(0);
   }, [navigate]);
 
-  /// refreshing, try to make cond to stop refreshing after click
   return { history, clearHistory };
 };
