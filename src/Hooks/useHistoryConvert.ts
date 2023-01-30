2e394440b506813afea11346cda0b3cbab1856bd
TadeQ <tadeuszkiszka1@gmail.com>
2023-01-30 20:08:39 +0100

    chore: add necessary packages

--- src/Hooks/useHistoryConvert.ts
+++ src/Hooks/useHistoryConvert.ts
@@ -1,9 +1,9 @@
-import { useState, useEffect, useCallback } from "react";
-import { useQueryClient, useIsRestoring } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
-import { useLoaderData } from "react-router-dom";
+import { useState, useEffect, useCallback, useRef } from 'react';
+import { useQueryClient, useIsRestoring } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
-import { ConvertCurrenciesResponse } from "../services/CurrenciesServices";
+import { ConvertCurrenciesResponse } from '../services/CurrenciesServices';
 
 export const useHistoryConvert = () => {
   const queryClient = useQueryClient();
@@ -13,8 +13,55 @@ export const useHistoryConvert = () => {
 
   // const [history, setHistory] = useState<ConvertCurrenciesResponse[]>([]);
 
+  // const ref - useRef();
+
+  //   const test = {
+  //     wlasciowsc1: "tst",
+  //     wlasiciwcs2: "sdsd",
+  //   };
+
+  //   useEffect(() => {
+  //     console.log("c")
+  //   }); // co render
+
+  //   useEffect(() => {
+  //     console.log("c")
+  //   }, []); // po pierwszym renderze
+
+  //   useEffect(() => {
+  //     console.log("c")
+  //   }, [test.wlasciowsc1]);
+
+  //   useEffect(() => {
+  // (async () => {
+  //   fetch("/");
+  // })();
+
+  // const add = (x) => {
+  //   return (y) => {
+  //     return x+y;
+  //   }
+  // }
+
+  // add(1)(2);
+
+  // const addWith1 = add(1);
+
+  // const test = () => {
+  //   console.log("a");
+
+  //   setTimeout(() => {
+  //     console.log("b")
+  //   }, 0)
+
+  //   console.log("c")
+  // } // a, c, b
+
+  // addWith1(2)
+  //   }, [])
+
   const clearHistory = useCallback(() => {
-    queryClient.removeQueries({ queryKey: ["convert"] });
+    queryClient.removeQueries({ queryKey: ['convert'] });
     navigate(0);
   }, [navigate]);
 
